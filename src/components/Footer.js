e5a2aa6b83b39cb057002e83e8d76cee6a18b079
Asif Javed <90240025+Asifcreative@users.noreply.github.com>
2025-01-02 11:45:40 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
 <footer className="footer-area">
     <div className="Container text-center text-white py-4 mt-3 glassFooter">
-    <span>React Weather App By </span>
+    <span>Weather App Build with React By </span>
     <Link className="text-danger text-decoration-none fs-5" aria-current="page" to="/">ASIF JAVED</Link>
     <span> &reg; {new Date().getFullYear()}</span>
      
